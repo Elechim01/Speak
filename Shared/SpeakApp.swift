e5342ea164b47d25468419cb1dbd3121df01b104
Elechim01 <mikymanny015@gmail.com>
2021-05-01 19:02:03 +0200

    Initial Commit

---
+++ Shared/SpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 01/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
